8b76f4a2dc959879b201f1952c6bc7aca34c95d5
fabien lamarque <fabien.lamarque@gmail.com>
2025-12-09 16:09:50 +0100

    provider for firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,14 +7,15 @@ import {getAuth, provideAuth} from '@angular/fire/auth';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 import {environment} from '../environments/environment';
 
+const provideFirebase = () => [
+  provideFirebaseApp(() => initializeApp(environment.firebase)),
+  provideAuth(() => getAuth()),
+  provideFirestore(() => getFirestore())
+];
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    // 1. Initialisation de l'application Firebase
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    // 2. Fournisseur pour Authentication
-    provideAuth(() => getAuth()),
-    // 3. Fournisseur pour Cloud Firestore
-    provideFirestore(() => getFirestore())
+    provideFirebase()
   ]
 };
