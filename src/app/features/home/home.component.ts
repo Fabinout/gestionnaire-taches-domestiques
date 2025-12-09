b191281ca9a98b24bc6103c54003c20055f33cc5
fabien lamarque <fabien.lamarque@gmail.com>
2025-12-09 14:40:03 +0100

    ajout des taches domestiques à la home page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-import { AuthService, User } from '../../core/auth/auth.service'; // Ajustez le chemin si nécessaire
-import { Observable } from 'rxjs';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {AuthService, User} from '../../core/auth/auth.service'; // Ajustez le chemin si nécessaire
+import {Observable} from 'rxjs';
+import {CommonModule} from '@angular/common';
+import {TaskService} from '../tasks/services/task.service';
+import {Task} from '../tasks/models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -13,10 +15,13 @@ import { CommonModule } from '@angular/common';
 export class HomeComponent {
   // L'utilisateur est suivi via l'observable du service
   user$: Observable<User | null>;
+  tasks$: Observable<Task[]>;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private taskService: TaskService
+  ) {
     // Récupère l'observable de l'utilisateur
     this.user$ = this.authService.user$;
+    this.tasks$ = this.taskService.getAllTasks();
   }
 
   /**
